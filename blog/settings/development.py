b4ccb98c1ba70be36e882f1d93e300e48e011fa2
Isaac Bythewood <isaac@bythewood.me>
2022-06-16 00:20:11 +0000

    Change db location to get all WAL files

--- blog/settings/development.py
+++ blog/settings/development.py
@@ -34,7 +34,7 @@ CSRF_TRUSTED_ORIGINS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
